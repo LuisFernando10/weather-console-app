b492ec0a3ab4afb496b6017dd707071f5814473a
Luis Fernando <desarrollo@fcomunicaciones.com>
2021-07-15 11:05:36 -0500

    Se establece conección API a MapBox

--- models/busquedas.js
+++ models/busquedas.js
@@ -15,7 +15,7 @@
 
             try {
                 // petición HTTP
-                const response = await axios.get('https://reqres.in/api/users?page=2');
+                const response = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madr.json?access_token=pk.eyJ1IjoibGZjaGFtb3JybyIsImEiOiJja3Iybm54b2UyZGUxMnptZmR6aG5iZWhhIn0.3GdDWjch4joznMa1TuSKNg&limit=5&language=es');
                 console.log( response.data );
 
                 return []; // Retorna lugares que coincidan   
